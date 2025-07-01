a8b0c293dfaec36d9515db8ed0243326c7c6a5d0
mostafa-khodaparast <mostafa.khodaparast1997@gmail.com>
2025-07-01 12:47:09 +0330

    add put and delete api

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 api = FastAPI()
 
+
 all_todos = [
    {'todo_id': 1, 'todo_name': 'sports', 'todo_description': 'go to the gym'},
    {'todo_id': 2, 'todo_name': 'read', 'todo_description': 'go to the gym'},
@@ -10,12 +11,14 @@ all_todos = [
    {'todo_id': 5, 'todo_name': 'meditate', 'todo_description': 'go to the gym'}, 
 ]
 
+
 #pass parameter
 @api.get('/todos/{todo_id}')
 def get_todo(todo_id: int):
     for todo in all_todos:
         if todo['todo_id'] == todo_id:
             return { 'result': todo}
+
 
 #query parameter    
 #fastapi use pydantic, so type should be given to variables    
@@ -25,3 +28,35 @@ def get_todos(first_n: int = None):
         return all_todos[:first_n]
     else:
         return all_todos
+    
+
+@api.post('/todos')
+def create_todo(todo:dict):
+    new_todo_id = max(todo['todo_id'] for todo in all_todos)  +1
+
+    new_todo = {
+        'todo_id': new_todo_id,
+        'todo_name': todo['todo_name'],
+        'todo_description': todo['todo_description']
+    }
+
+    all_todos.append(new_todo)
+
+    return new_todo
+
+@api.put('/todos/{todo_id}')
+def update_todo(todo_id: int, updated_todo: dict):
+    for todo in all_todos:
+        if todo['todo_id'] == todo_id:
+            todo['todo_name'] = updated_todo['todo_name']
+            todo['todo_description'] = updated_todo['todo_description']
+            return todo
+    return 'Error, Not found...'
+
+@api.delete('/todo/{todo_id}')
+def delete_todo(todo_id: int):
+    for index, todo in enumerate(all_todos):
+        if todo['todo_id'] == todo_id:
+            deleted_todo = all_todos.pop(index)
+            return delete_todo
+    return "Error, Not found..."
